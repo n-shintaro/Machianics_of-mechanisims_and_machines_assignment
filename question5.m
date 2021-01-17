157c4c1e2e2c7dcc648053c3a376cbacd464468c
nakaoka <nakaoka.shintaro.infratop@gmail.com>
2021-01-18 00:32:45 +0900

    question5 finished

--- question5.m
+++ question5.m
@@ -22,6 +22,7 @@ l_1=1.0;
 l_2=2+368/1000;
 th_1=0:2*pi:0.1;
 syms th_1 th_2
+figure(1)
 equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
 fimplicit(equation,[0 2*pi 0 2*pi])
 title('2D plot of C')
@@ -32,30 +33,76 @@ grid on
 a=linspace(0, 2*pi,100);
 b=linspace(0, 2*pi,100);
 [A,B]=meshgrid(a,b); 
-figure
-subplot(2,2,1)
+figure(2)
 x=(l_2+l_1.*cos(A)).*cos(B);
 y=(l_2+l_1.*cos(A)).*sin(B);
 z=l_1.*sin(A);
 surf(x,y,z);
 hold on
 % draw the configuration on the torus
-th_1=linspace(0, 2*pi,100);
-size=numel(th_1);
-Th1=zeros(size,1);
-Th2=zeros(size,1);
+
+size=10000;
+error=1e-5;
 
 for i=1:size
-    Th1(i)=th_1(i);
-    equation=l_1.*(1-cos(Th1(i)))+l_2*(-cos(Th1(i)).*cos(th_2)-sin(Th1(i)).*sin(th_2)+cos(th_2))==0;
-    Th2(i)=vpasolve(equation, th_2,[0 2*pi]);
+    th_1=2*pi/size*i;
+    for j=1:size
+        th_2=2*pi/size*j;
+        if abs(l_1*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2)))<error
+            Th1=th_1;
+            Th2=th_2;
+            X=(l_2+l_1.*cos(Th1)).*cos(Th2);
+            Y=(l_2+l_1.*cos(Th1)).*sin(Th2);
+            Z=l_1.*sin(Th1);
+            plot3(X,Y,Z,'r+');
+        end
+        
+    end
 end
 
-X=(l_2+l_1.*cos(Th1)).*cos(Th2);
-Y=(l_2+l_1.*cos(Th1)).*sin(Th2);
-Z=l_1.*sin(Th1);
-plot3(X,Y,Z,'r+');
 
-subplot(2,1,2);
-plot(Th1,Th2,'r+');
+
+% subplot(2,1,2);
+% plot(Th1,Th2,'r+');
+
+
+
+%% 
+%I try to use the function "vpasolve" 
+%but In the case of non-polynomial equations, vpasolve returns the first solution found.
+% so I can get all the feasible soultions.
+
+% grid on 
+% % draw the Torus
+% a=linspace(0, 2*pi,100);
+% b=linspace(0, 2*pi,100);
+% [A,B]=meshgrid(a,b); 
+% figure
+% subplot(2,2,1)
+% x=(l_2+l_1.*cos(A)).*cos(B);
+% y=(l_2+l_1.*cos(A)).*sin(B);
+% z=l_1.*sin(A);
+% surf(x,y,z);
+% hold on
+% % draw the configuration on the torus
+% th_1=linspace(0, 2*pi,100);
+% size=numel(th_1);
+% Th1=zeros(size,1);
+% Th2=zeros(size,1);
+% 
+% for i=1:size
+%     Th1(i)=th_1(i);
+%     equation=l_1.*(1-cos(Th1(i)))+l_2*(-cos(Th1(i)).*cos(th_2)-sin(Th1(i)).*sin(th_2)+cos(th_2))==0;
+%     Th2(i)=vpasolve(equation, th_2,[0 2*pi]);
+% end
+% 
+% X=(l_2+l_1.*cos(Th1)).*cos(Th2);
+% Y=(l_2+l_1.*cos(Th1)).*sin(Th2);
+% Z=l_1.*sin(Th1);
+% plot3(X,Y,Z,'r+');
+% 
+% subplot(2,1,2);
+% plot(Th1,Th2,'r+');
+
+
 
--- question6.m
+++ question6.m
@@ -71,35 +71,54 @@ l_2=2+368/1000;
 % syms th_2
 % equation=l_1.*(1-cos(Th1_3))+l_2*(-cos(Th1_3).*cos(th_2)-sin(Th1_3).*sin(th_2)+cos(th_2))==0;
 % Th2_3=vpasolve(equation, th_2,[0 2*pi])
-
+% 
 th_1=linspace(0, 3*pi/2,4);
-size=numel(th_1);
-Th1=zeros(size,1);
+th_2=linspace(0, 2*pi,1000);
+size=numel(th_2);
+error=1e-2;
 Th2=zeros(size,1);
 answer=zeros(8,2);
 % th_1=(0, pi/2,pi,3*pi/2)
-for i=1:size
-    Th1(i)=th_1(i);
-    answer(i,1)=Th1(i);
-    syms th_2
-    equation=l_1.*(1-cos(Th1(i)))+l_2*(-cos(Th1(i)).*cos(th_2)-sin(Th1(i)).*sin(th_2)+cos(th_2))==0;
-    Th2(i)=vpasolve(equation, th_2,[0 2*pi])
-    answer(i,2)=Th2(i);
+
+
+for i=1:4
+    for j=1:size
+        Th1=th_1(i);
+        Th2=th_2(j);
+        if abs(l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2)))<error
+            plot(th_1(i),Th2,'r+');
+            hold on
+       end
+    end    
 end
-% 
+th_1=linspace(0, 2*pi,1000);
+equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
+fimplicit(equation,th_2,[0 2*pi 0 2*pi])
+    %
 % th_2=linspace(0, 3*pi/2,4);
 % size=numel(th_2);
 % Th1_2=zeros(size,1);
 % Th2_2=zeros(size,1);
 % 
-% % th_2=(0, pi/2,pi,3*pi/2)
-% for i=1:size
-%     Th2_2(i)=th_2(i);
-%     answer(i+4,2)=Th2_2(i);
-%     syms th_1
-%     equation2=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(Th2_2(i))-sin(th_1).*sin(Th2_2(i))+cos(Th2_2(i)))==0;
-%     Th1_2(i)=vpasolve(equation2, th_1,[0 2*pi])
-%     answer(i+4,1)=Th1_2(i);
+% th_2=(0, pi/2,pi,3*pi/2)
+
+
+
+
+% size=1000;
+% th_1=linspace(0, 2*pi,size);
+% th_2=linspace(0, 3*pi/2,4);
+% 
+% error=1e-2;
+% for i=1:4
+%     Th2_2=th_2(i);
+%     for j=1:size
+%         if abs(l_1*(1-cos(th_1(j)))+l_2*(-cos(th_1(j)).*cos(Th2_2)-sin(th_1(j)).*sin(Th2_2)+cos(Th2_2)))<error
+%             plot(th_1(j),Th2_2,'r+');
+%             j
+%             hold on
+%         end
+%     end
 % end
 
 
