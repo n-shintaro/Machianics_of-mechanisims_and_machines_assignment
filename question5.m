1494a34fb6edb36edca2f67371b5b618e4a73106
nakaoka <nakaoka.shintaro.infratop@gmail.com>
2021-01-19 03:04:05 +0900

    modify all

--- question5.m
+++ question5.m
@@ -22,7 +22,6 @@ set(groot,'DefaultAxesLineStyleOrder',style,'defaultAxesColorOrder',color)
 %In the case of non-polynomial equations, vpasolve returns the first solution found.
 % therefore, proof by cases
 
-figure_number=1;
 l_1=1.0;
 l_2=2+368/1000;
 th_2_0=linspace(-pi, pi,100);
@@ -78,13 +77,13 @@ Z_negative=l_1.*sin(th_1_negative);
 a=linspace(-pi,pi,100);
 b=linspace(-pi, pi,100);
 [A,B]=meshgrid(a,b); 
+figure(2)
 x=(l_2+l_1.*cos(A)).*cos(B);
 y=(l_2+l_1.*cos(A)).*sin(B);
 z=l_1.*sin(A);
 
 %draw the graph
-figure(figure_number)
-figure_number=figure_number+1;
+figure(1)
 th_1=0:2*pi:0.1;
 syms th_1 th_2
 equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
@@ -109,9 +108,7 @@ grid on
 
 
 %draw the configuration on the torus
-
-figure(figure_number)
-figure_number=figure_number+1;
+figure(2)
 plot3(X_0,Y_0,Z_0,'r+');
 hold on
 plot3(X_positive,Y_positive,Z_positive,'r+');
@@ -123,187 +120,6 @@ zlabel('z')
 grid on
 hold off
 
-%% Q6
-% each feasible configuration which has either a θ1=0
-th_2_q6_0=linspace(-pi, pi,100);
-th_1_q6_0=zeros(1,length(th_2_q6_0));
-
-% each feasible configuration which has either a θ1 value in {pi/2, pi, 3pi/2} 
-th_1_q6=[pi/2,pi,3*pi/2];
-size=length(th_1_q6);
-th_2_q6=zeros(1,size);
-for i=1:size
-    Th1=th_1_q6(i);
-    syms Th2
-    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
-    th_2_q6(i)=vpasolve(equation, Th2,[0 pi]);
-end
-
-th_2_q6_2=zeros(1,size);
-for i=1:size
-    Th1=th_1_q6(i);
-    syms Th2
-    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
-    th_2_q6_2(i)=vpasolve(equation, Th2,[pi 2*pi]);
-end
-
-
-X_q6_0=(l_2+l_1.*cos(th_1_q6_0)).*cos(th_2_q6_0);
-Y_q6_0=(l_2+l_1.*cos(th_1_q6_0)).*sin(th_2_q6_0);
-Z_q6_0=l_1.*sin(th_1_q6_0);
-
-X_q6=(l_2+l_1.*cos(th_1_q6)).*cos(th_2_q6);
-Y_q6=(l_2+l_1.*cos(th_1_q6)).*sin(th_2_q6);
-Z_q6=l_1.*sin(th_1_q6);
-
-X_q6_2=(l_2+l_1.*cos(th_1_q6)).*cos(th_2_q6_2);
-Y_q6_2=(l_2+l_1.*cos(th_1_q6)).*sin(th_2_q6_2);
-Z_q6_2=l_1.*sin(th_1_q6);
-% 2D plot
-
-figure(figure_number)
-figure_number=figure_number+1;
-
-th_1=0:2*pi:0.1;
-syms th_1 th_2
-equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
-fimplicit(equation,[0 2*pi 0 2*pi],'r')
-hold on
-plot(th_1_q6_0,th_2_q6_0,'bo','MarkerSize',10)
-plot(th_1_q6,th_2_q6,'bo','MarkerSize',10)
-plot(th_1_q6,th_2_q6_2,'bo','MarkerSize',10)
-xlim([0,2*pi])
-ylim([0,2*pi])
-xlabel('theta_1')
-ylabel('theta_2')
-grid on
-
-% 3D plot
-
-figure(figure_number)
-figure_number=figure_number+1;
-plot3(X_0,Y_0,Z_0,'r+');
-hold on
-plot3(X_positive,Y_positive,Z_positive,'r+');
-plot3(X_negative,Y_negative,Z_negative,'r+');
-surf(x,y,z);
-plot3(X_q6_0,Y_q6_0,Z_q6_0,'bo');
-plot3(X_q6,Y_q6,Z_q6,'bo');
-plot3(X_q6_2,Y_q6_2,Z_q6_2,'bo');
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
-grid on
-hold off
-
-for i=1:length(th_1_q6)
-        figure(figure_number)
-        figure_number=figure_number+1;
-        str = {'O_1','O_2','P_2','P_1','O_1'};
-        P1=[l_1*cos(th_1_q6(i)),l_1*sin(th_1_q6(i))];
-        P2=[l_1+l_2*cos(th_2_q6(i)),l_2*sin(th_2_q6(i))];
-        p1=[0 l_1 P2(1) P1(1) 0];
-        p2=[0 0 P2(2) P1(2) 0];
-        plot(p1,p2,'ro');
-        hold on
-        plot(p1,p2);
-        text(p1,p2,str);
-        xlim([-3 3])
-        ylim([-3 3])
-        grid on
-        hold off
-    
-end
-
-for i=1:length(th_1_q6)
-        figure(figure_number)
-        figure_number=figure_number+1;
-        str = {'O_1','O_2','P_2','P_1','O_1'};
-        P1=[l_1*cos(th_1_q6(i)),l_1*sin(th_1_q6(i))];
-        P2=[l_1+l_2*cos(th_2_q6_2(i)),l_2*sin(th_2_q6_2(i))];
-        p1=[0 l_1 P2(1) P1(1) 0];
-        p2=[0 0 P2(2) P1(2) 0];
-        plot(p1,p2,'ro');
-        hold on
-        plot(p1,p2);
-        text(p1,p2,str);
-        xlim([-3 3])
-        ylim([-3 3])
-        grid on
-        hold off
-    
-end
-
-
-% each feasible configuration which has either a θ2 value in {0 pi/2, pi, 3pi/2} 
-% th_2_q6_3=[0,0,0,pi/2,pi/2,pi/2,pi,pi,pi,3*pi/2,3*pi/2,3*pi/2];
-% size=length(th_2_q6_3);
-% th_1_q6_3=zeros(1,size*3);
-% th_1_q6_4=zeros(1,size*3);
-% 
-% for i=1:size
-%     for j=1:3
-%         Th2=th_2_q6_3(i);
-%         syms Th1
-%         equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
-%         th_1_q6_3(i*j)=vpasolve(equation, Th1,[0,3*pi/2],'Random',true);
-%     end
-% end
-% 
-% for i=1:size
-%     for j=1:3
-%         Th2=th_2_q6_3(i);
-%         syms Th1
-%         equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
-%         th_1_q6_4(i*j)=vpasolve(equation, Th1,[pi/2,2*pi],'Random',true);
-%     end
-% end
-% % 
-% X_q6_3=(l_2+l_1.*cos(th_1_q6_3)).*cos(th_2_q6_3);
-% Y_q6_3=(l_2+l_1.*cos(th_1_q6_3)).*sin(th_2_q6_3);
-% Z_q6_3=l_1.*sin(th_1_q6_3);
-% 
-% X_q6_4=(l_2+l_1.*cos(th_1_q6_4)).*cos(th_2_q6_4);
-% Y_q6_4=(l_2+l_1.*cos(th_1_q6_4)).*sin(th_2_q6_4);
-% Z_q6_4=l_1.*sin(th_2_q6_4);
-% 
-% % 2D plot
-% 
-% 
-% 
-% th_1=-pi:pi:0.1;
-% syms th_1 th_2
-% equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
-% fimplicit(equation,[0 2*pi 0 2*pi],'r')
-% 
-% hold on
-% plot(th_1_q6_3,th_2_q6_3,'bo','MarkerSize',10)
-% plot(th_1_q6_4,th_2_q6_3,'bo','MarkerSize',10)
-% xlim([0,2*pi])
-% ylim([0,2*pi])
-% xlabel('theta_1')
-% ylabel('theta_2')
-% grid on
-% 
-% hold off
-% 
-% 
-% % 3D plot
-% 
-% figure(figure_number)
-% figure_number=figure_number+1;
-% plot3(X_0,Y_0,Z_0,'r+');
-% hold on
-% plot3(X_positive,Y_positive,Z_positive,'r+');
-% plot3(X_negative,Y_negative,Z_negative,'r+');
-% surf(x,y,z);
-% plot3(X_q6_3,Y_q6_3,Z_q6_3,'bo');
-% plot3(X_q6_4,Y_q6_4,Z_q6_4,'bo');
-% xlabel('X')
-% ylabel('Y')
-% zlabel('Z')
-% grid on
-% hold off
 %% Q7
 
 % to solve the simultaneous equations
@@ -311,9 +127,7 @@ syms th_1 th_2
 equation1=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
 equation2=l_1.*sin(th_1)+l_2*(-sin(th_2-th_1));
 equation3=-l_2.*sin(th_2)+l_2*(sin(th_2-th_1));
-
-figure(figure_number)
-figure_number=figure_number+1;
+figure(3)
 
 fimplicit(equation1,[-pi pi])
 hold on
@@ -342,10 +156,7 @@ th_1_c=[0,0,0];
 th_2_c=[0,pi,-pi];
 
 % draw the answer on 2D plot
-
-figure(figure_number)
-figure_number=figure_number+1;
-
+figure(4)
 th_1=0:2*pi:0.1;
 syms th_1 th_2
 equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
@@ -375,10 +186,7 @@ X_c=(l_2+l_1.*cos(th_1_c)).*cos(th_2_c);
 Y_c=(l_2+l_1.*cos(th_1_c)).*sin(th_2_c);
 Z_c=l_1.*sin(th_1_c);
 
-
-figure(figure_number)
-figure_number=figure_number+1;
-
+figure(5)
 plot3(X_0,Y_0,Z_0,'r+');
 hold on
 plot3(X_positive,Y_positive,Z_positive,'r+');
@@ -395,26 +203,8 @@ zlabel('z')
 grid on
 legend('','','configuration space','(a)','(b)','(c)','torus')
 hold off
-th_1=[0,0,0];
-th_2=[0,-pi,pi];
-for i=1:length(th_1)
-        figure(figure_number)
-        figure_number=figure_number+1;
-        str = {'O_1','O_2','P_2','P_1','O_1'};
-        P1=[l_1*cos(th_1(i)),l_1*sin(th_1(i))];
-        P2=[l_1+l_2*cos(th_2(i)),l_2*sin(th_2(i))];
-        p1=[0 l_1 P2(1) P1(1) 0];
-        p2=[0 0 P2(2) P1(2) 0];
-        plot(p1,p2,'ro');
-        hold on
-        plot(p1,p2);
-        text(p1,p2,str);
-        xlim([-3 3])
-        ylim([-3 3])
-        grid on
-        hold off
-    
-end
+
+
 
 
 %% Another soultion of Q5
--- question5_7_2.m
+++ question5_7_2.m
@@ -22,12 +22,13 @@ set(groot,'DefaultAxesLineStyleOrder',style,'defaultAxesColorOrder',color)
 %In the case of non-polynomial equations, vpasolve returns the first solution found.
 % therefore, proof by cases
 
+figure_number=1;
 l_1=1.0;
 l_2=2+368/1000;
 th_2_0=linspace(-pi, pi,100);
 size=numel(th_2_0);
-Th1=zeros(size,1);
-Th2=zeros(size,1);
+Th1=zeros(1,size);
+Th2=zeros(1,size);
 
 for i=1:size
     Th2=th_2_0(i);
@@ -77,13 +78,13 @@ Z_negative=l_1.*sin(th_1_negative);
 a=linspace(-pi,pi,100);
 b=linspace(-pi, pi,100);
 [A,B]=meshgrid(a,b); 
-figure(2)
 x=(l_2+l_1.*cos(A)).*cos(B);
 y=(l_2+l_1.*cos(A)).*sin(B);
 z=l_1.*sin(A);
 
 %draw the graph
-figure(1)
+figure(figure_number)
+figure_number=figure_number+1;
 th_1=0:2*pi:0.1;
 syms th_1 th_2
 equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
@@ -108,7 +109,9 @@ grid on
 
 
 %draw the configuration on the torus
-figure(2)
+
+figure(figure_number)
+figure_number=figure_number+1;
 plot3(X_0,Y_0,Z_0,'r+');
 hold on
 plot3(X_positive,Y_positive,Z_positive,'r+');
@@ -120,39 +123,175 @@ zlabel('z')
 grid on
 hold off
 
+%% Q6
+% each feasible configuration which has either a θ1=0
+th_2_q6_0=linspace(-pi, pi,100);
+th_1_q6_0=zeros(1,length(th_2_q6_0));
+
+% each feasible configuration which has either a θ1 value in {pi/2, pi, 3pi/2} 
+th_1_q6=[pi/2,pi,3*pi/2];
+size=length(th_1_q6);
+th_2_q6=zeros(1,size);
+for i=1:size
+    Th1=th_1_q6(i);
+    syms Th2
+    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
+    th_2_q6(i)=vpasolve(equation, Th2,[0 pi]);
+end
+
+th_2_q6_2=zeros(1,size);
+for i=1:size
+    Th1=th_1_q6(i);
+    syms Th2
+    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
+    th_2_q6_2(i)=vpasolve(equation, Th2,[pi 2*pi]);
+end
+
+
+X_q6_0=(l_2+l_1.*cos(th_1_q6_0)).*cos(th_2_q6_0);
+Y_q6_0=(l_2+l_1.*cos(th_1_q6_0)).*sin(th_2_q6_0);
+Z_q6_0=l_1.*sin(th_1_q6_0);
+
+X_q6=(l_2+l_1.*cos(th_1_q6)).*cos(th_2_q6);
+Y_q6=(l_2+l_1.*cos(th_1_q6)).*sin(th_2_q6);
+Z_q6=l_1.*sin(th_1_q6);
+
+X_q6_2=(l_2+l_1.*cos(th_1_q6)).*cos(th_2_q6_2);
+Y_q6_2=(l_2+l_1.*cos(th_1_q6)).*sin(th_2_q6_2);
+Z_q6_2=l_1.*sin(th_1_q6);
+% 2D plot
+
+figure(figure_number)
+figure_number=figure_number+1;
+
+th_1=0:2*pi:0.1;
+syms th_1 th_2
+equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
+fimplicit(equation,[0 2*pi 0 2*pi],'r')
+hold on
+plot(th_1_q6_0,th_2_q6_0,'bo','MarkerSize',10)
+plot(th_1_q6,th_2_q6,'bo','MarkerSize',10)
+plot(th_1_q6,th_2_q6_2,'bo','MarkerSize',10)
+xlim([0,2*pi])
+ylim([0,2*pi])
+xlabel('theta_1')
+ylabel('theta_2')
+grid on
+
+% 3D plot
+
+figure(figure_number)
+figure_number=figure_number+1;
+plot3(X_0,Y_0,Z_0,'r+');
+hold on
+plot3(X_positive,Y_positive,Z_positive,'r+');
+plot3(X_negative,Y_negative,Z_negative,'r+');
+surf(x,y,z);
+plot3(X_q6_0,Y_q6_0,Z_q6_0,'bo');
+plot3(X_q6,Y_q6,Z_q6,'bo');
+plot3(X_q6_2,Y_q6_2,Z_q6_2,'bo');
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+grid on
+
+% each feasible configuration which has either a θ2 value in {pi/2, pi, 3pi/2} 
+th_2_q6_3=[0 pi/2,pi,3*pi/2];
+size=length(th_2_q6_3);
+th_1_q6_3=zeros(1,size);
+for i=1:size
+    Th2=th_2_q6_3(i);
+    syms Th1
+    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
+    th_2_q6_3(i)=vpasolve(equation, Th1,[0 pi]);
+end
+
+th_1_q6_4=zeros(1,size);
+for i=1:size
+    Th2=th_2_q6_3;
+    syms Th1
+    equation=l_1.*(1-cos(Th1))+l_2*(-cos(Th1).*cos(Th2)-sin(Th1).*sin(Th2)+cos(Th2))==0;
+    th_1_q6_4(i)=vpasolve(equation, Th1,[-pi 0]);
+end
+
+% 2D plot
+
+figure(figure_number)
+figure_number=figure_number+1;
+
+th_1=0:2*pi:0.1;
+syms th_1 th_2
+equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
+fimplicit(equation,[0 2*pi 0 2*pi],'r')
+hold on
+plot(th_1_q6_3,th_2_q6_3,'bo','MarkerSize',10)
+plot(th_1_q6_4,th_2_q6_3,'bo','MarkerSize',10)
+xlim([0,2*pi])
+ylim([0,2*pi])
+xlabel('theta_1')
+ylabel('theta_2')
+grid on
+
+
 %% Q7
 
+% to solve the simultaneous equations
+syms th_1 th_2
+equation1=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
+equation2=l_1.*sin(th_1)+l_2*(-sin(th_2-th_1));
+equation3=-l_2.*sin(th_2)+l_2*(sin(th_2-th_1));
+
+figure(figure_number)
+figure_number=figure_number+1;
+
+fimplicit(equation1,[-pi pi])
+hold on
+fimplicit(equation2,[-pi pi])
+fimplicit(equation3,[-pi pi])
+xlabel('theta1')
+ylabel('theta2')
+legend('(1)','(2)','(3)')
+grid on
+hold off
+
+
+
 % answer of (a)
-th_2_a=linspace(-pi, pi,100);
-th_1_a=zeros(length(th_2_a),1);
+th_1_a=[0,0,0];
+th_2_a=[0,pi,-pi];
 
 
 % answer of (b)
-th_1_b=[0,0,0];
-th_2_b=[0,pi,-pi];
+th_2_b=linspace(-pi, pi,100);
+th_1_b=zeros(1,length(th_2_b));
+
 
 % answer of (c)
-th_2_c=linspace(-pi, pi,100);
-th_1_c=zeros(length(th_2_a),1);
+th_1_c=[0,0,0];
+th_2_c=[0,pi,-pi];
 
 % draw the answer on 2D plot
-figure(4)
+
+figure(figure_number)
+figure_number=figure_number+1;
+
 th_1=0:2*pi:0.1;
 syms th_1 th_2
 equation=l_1.*(1-cos(th_1))+l_2*(-cos(th_1).*cos(th_2)-sin(th_1).*sin(th_2)+cos(th_2));
 fimplicit(equation,[-pi pi -pi pi],'r')
 hold on
-plot(th_1_a,th_2_a,'b+','MarkerSize',10)
-plot(th_1_b,th_2_b,'ko','MarkerSize',15)
-plot(th_1_c,th_2_c,'g+','MarkerSize',5)
+plot(th_1_a,th_2_a,'bo','MarkerSize',10)
+plot(th_1_b,th_2_b,'y+','MarkerSize',5)
+plot(th_1_c,th_2_c,'go','MarkerSize',15)
 xlim([-pi,pi])
 ylim([-pi,pi])
 xlabel('theta_1')
 ylabel('theta_2')
 legend('configuration space','(a)','(b)','(c)')
 grid on
+hold off
 
-%%draw the answer on the torus
+%draw the answer on the torus
 X_a=(l_2+l_1.*cos(th_1_a)).*cos(th_2_a);
 Y_a=(l_2+l_1.*cos(th_1_a)).*sin(th_2_a);
 Z_a=l_1.*sin(th_1_a);
@@ -166,20 +305,24 @@ Y_c=(l_2+l_1.*cos(th_1_c)).*sin(th_2_c);
 Z_c=l_1.*sin(th_1_c);
 
 
-figure(5)
+figure(figure_number)
+figure_number=figure_number+1;
+
 plot3(X_0,Y_0,Z_0,'r+');
 hold on
 plot3(X_positive,Y_positive,Z_positive,'r+');
 plot3(X_negative,Y_negative,Z_negative,'r+');
-plot3(X_a,Y_a,Z_a,'b+','MarkerSize',15);
-plot3(X_b,Y_b,Z_b,'k+','MarkerSize',10);
-plot3(X_c,Y_c,Z_c,'g+','MarkerSize',5);
-legend('configuration space','configuration space','(a)','(b)','(c)')
+plot3(X_a,Y_a,Z_a,'bo','MarkerSize',10);
+plot3(X_b,Y_b,Z_b,'y+','MarkerSize',5);
+plot3(X_c,Y_c,Z_c,'go','MarkerSize',15);
+
 surf(x,y,z);
 xlabel('x')
 ylabel('y')
 zlabel('z')
+
 grid on
+legend('','','configuration space','(a)','(b)','(c)','torus')
 hold off
 
 
